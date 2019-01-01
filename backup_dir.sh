619f8ceda89489c4d2054a29efd83d1d8f9cf2f1
Ioan-Cristian Barbu <barbu.cristian13@gmail.com>
2019-01-01 16:07:50 +0200

    Initial commit

---
+++ backup_dir.sh
@@ -0,0 +1,9 @@
+# Get backup source
+# Get backup destination; default Data\sourceFolderName
+
+# Check if Data is mounted; If not, mount;
+
+# Copy all files/folders older than x days.
+
+# ++add safety check to make sure same count of items coppied as will be deleted;
+# Delete all files/folders older than x days.
